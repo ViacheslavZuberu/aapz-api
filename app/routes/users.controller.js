9db5389a7705fce2237e6bd2e4b50d8db1c0a335
ViacheslavSheviakov <viacheslav.sheviakov@nure.ua>
2019-06-17 00:58:56 +0300

    User upgrade

--- app/routes/users.controller.js
+++ app/routes/users.controller.js
@@ -1,66 +1,78 @@
-﻿const express = require('express');
+﻿const express = require("express");
 const router = express.Router();
-const userService = require('../services/user.service');
-const authorize = require('../_helpers/authorize')
-const Role = require('../_helpers/role');
+const userService = require("../services/user.service");
+const authorize = require("../_helpers/authorize");
+const Role = require("../_helpers/role");
 
 // routes
-router.post('/authenticate', authenticate);     // public route
-router.post('/register', register);             // public route
-router.delete('/remove/:id', authorize(Role.Admin), deleteUser);
-router.get('/', authorize(Role.Admin), getAll); // admin only
-router.get('/:id', authorize(), getById);       // all authenticated users
+router.post("/authenticate", authenticate); // public route
+router.post("/register", register); // public route
+router.post("/upgrade/:id", authorize(Role.Admin), upgradeUser);
+router.delete("/remove/:id", authorize(Role.Admin), deleteUser);
+router.get("/", authorize(Role.Admin), getAll); // admin only
+router.get("/:id", authorize(), getById); // all authenticated users
 module.exports = router;
 
 function authenticate(req, res, next) {
-    userService.authenticate(req.body)
-        .then(
-            user => user ?
-             res.json(user) : 
-             res.status(400).json({ 
-                 message: 'Username or password is incorrect' 
-             })
-        )
-        .catch(err => next(err));
+  userService
+    .authenticate(req.body)
+    .then(user =>
+      user
+        ? res.json(user)
+        : res.status(400).json({
+            message: "Username or password is incorrect"
+          })
+    )
+    .catch(err => next(err));
 }
 
 function register(req, res, next) {
-    userService
+  userService
     .register(req.body)
-    .then(
-        ok => ok ?
-         res.json({ message: "OK"}) : 
-         res.status(400).json({ 
-             message: 'Username or password is incorrect' 
-         })
+    .then(ok =>
+      ok
+        ? res.json({ message: "OK" })
+        : res.status(400).json({
+            message: "Username or password is incorrect"
+          })
     )
     .catch(err => next(err));
 }
 
 function deleteUser(req, res, next) {
-    userService
-        .removeUser(req.params.id)
-        .then(deletedUser => res.status(200).json(deletedUser))
-        .catch(err => next(err));
+  userService
+    .removeUser(req.params.id)
+    .then(deletedUser => res.status(200).json(deletedUser))
+    .catch(err => next(err));
 }
 
 function getAll(req, res, next) {
-    userService.getAll()
-        .then(users => res.json(users))
-        .catch(err => next(err));
+  userService
+    .getAll()
+    .then(users => res.json(users))
+    .catch(err => next(err));
 }
 
 function getById(req, res, next) {
-    const currentUser = req.user;
-    const id = parseInt(req.params.id);
+  const currentUser = req.user;
+  const id = parseInt(req.params.id);
 
-    // only allow admins to access other user records
-    if (id !== currentUser.sub && currentUser.role !== Role.Admin) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
+  // only allow admins to access other user records
+  if (id !== currentUser.sub && currentUser.role !== Role.Admin) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    userService
-        .getById(req.params.id)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+  userService
+    .getById(req.params.id)
+    .then(user => (user ? res.json(user) : res.sendStatus(404)))
+    .catch(err => next(err));
+}
+
+function upgradeUser(req, res, next) {
+  const id = req.params.id;
+
+  userService
+    .upgradeUser(id)
+    .then(user => (user ? res.json(user) : res.sendStatus(404)))
+    .catch(err => next(err));
 }
--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,121 +1,129 @@
-﻿const config = require('config.json');
-const jwt = require('jsonwebtoken');
+﻿const config = require("config.json");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
-const bcrypt= require("bcrypt");
-const Role = require('../_helpers/role');
+const bcrypt = require("bcrypt");
+const Role = require("../_helpers/role");
 const mongoose = require("mongoose");
 
 module.exports = {
-    authenticate,
-    getAll,
-    getById,
-    register,
-    removeUser
+  authenticate,
+  getAll,
+  getById,
+  register,
+  removeUser,
+  upgradeUser
 };
 
 async function authenticate({ username, password }) {
+  const user = await User.findOne({
+    username
+  }).lean();
 
-    const user = await User.findOne({
-        username
-    }).lean();
+  let userPass = password;
 
-    let userPass = password;
+  if (user) {
+    const correctPass = bcrypt.compareSync(userPass, user.password);
 
-    if (user) {
-        const correctPass = bcrypt.compareSync(
-            userPass, 
-            user.password
-        );
-
-        if (!correctPass) {
-            throw "Password is incorrect!";
-        }
-
-        const token = jwt.sign({ 
-            sub: user._id,
-            role: user.role
-        }, config.secret);
-
-        const { password, __v, ...userWithoutPassword } = user;
-
-        return {
-            ...userWithoutPassword,
-            token
-        };
-    }
-}
-
-async function register({
-    username, 
-    password,
-    email,
-    firstname,
-    lastname,
-    birth_date
-}) {
-    const existing = await User.findOne({username}).lean();
-
-    if (existing) {
-        throw "User already exists!";
+    if (!correctPass) {
+      throw "Password is incorrect!";
     }
 
-    let userBase = { 
-        _id: new mongoose.Types.ObjectId(),
-        username,
-        password: bcrypt.hashSync(password, 10),
-        firstname,
-        lastname,
-        role: Role.User
-    };
-
-    if (email) {
-        userBase.email = email;
-    }
-
-    if (birth_date) {
-        userBase.birth_date = birth_date;
-    }
-
-    let newUser = new User(userBase);
-
-    newUser
-        .save()
-        .catch(err => console.log(err));
-
-    return true;
-}
-
-async function removeUser(userId) {
-    let deletedUser = await User.findByIdAndDelete(userId);
-    let result = { 
-        message: "There's no user with such ID!"
-    };
-
-    if (deletedUser) {
-        const {__v, password, ...safeUser} = deletedUser;
-        result = safeUser;
-    }
-
-    return result;
-}
-
-async function getAll() {
-    let users = await User.find().lean();
-
-    return users.map(u => {
-        const { password, __v, ...userWithoutPassword } = u;
-        return userWithoutPassword;
-    });
-}
-
-async function getById(id) {
-    const user = await User.findOne({ _id: id }).lean();
-
-    if (!user) {
-        throw `There is no user with id: ${id}`;
-    }
+    const token = jwt.sign(
+      {
+        sub: user._id,
+        role: user.role
+      },
+      config.secret
+    );
 
     const { password, __v, ...userWithoutPassword } = user;
 
+    return {
+      ...userWithoutPassword,
+      token
+    };
+  }
+}
+
+async function register({
+  username,
+  password,
+  email,
+  firstname,
+  lastname,
+  birth_date
+}) {
+  const existing = await User.findOne({ username }).lean();
+
+  if (existing) {
+    throw "User already exists!";
+  }
+
+  let userBase = {
+    _id: new mongoose.Types.ObjectId(),
+    username,
+    password: bcrypt.hashSync(password, 10),
+    firstname,
+    lastname,
+    role: Role.User
+  };
+
+  if (email) {
+    userBase.email = email;
+  }
+
+  if (birth_date) {
+    userBase.birth_date = birth_date;
+  }
+
+  let newUser = new User(userBase);
+
+  newUser.save().catch(err => console.log(err));
+
+  return true;
+}
+
+async function removeUser(userId) {
+  let deletedUser = await User.findByIdAndDelete(userId);
+  let result = {
+    message: "There's no user with such ID!"
+  };
+
+  if (deletedUser) {
+    const { __v, password, ...safeUser } = deletedUser;
+    result = safeUser;
+  }
+
+  return result;
+}
+
+async function getAll() {
+  let users = await User.find().lean();
+
+  return users.map(u => {
+    const { password, __v, ...userWithoutPassword } = u;
     return userWithoutPassword;
+  });
+}
+
+async function getById(id) {
+  const user = await User.findOne({ _id: id }).lean();
+
+  if (!user) {
+    throw `There is no user with id: ${id}`;
+  }
+
+  const { password, __v, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+}
+
+async function upgradeUser(id) {
+  const user = await User.findByIdAndUpdate(id, {
+    $set: {
+      role: Role.MeetupManager
+    }
+  }).select("-__v -password");
+
+  return user;
 }
